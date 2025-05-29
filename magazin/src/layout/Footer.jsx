7f17acefa59c5d6c7684e6c1f9daa5ebba010487
Shadow-fiish <miky48731@mail.ru>
2025-05-29 13:48:57 +0300

    Resolved merge conflicts

---
+++ magazin/src/layout/Footer.jsx
@@ -0,0 +1,12 @@
+export function Footer () {
+    return (
+        <footer class="page-footer light-green accent-3">
+          <div class="footer-copyright">
+            <div class="container">
+            © {new Date().getFullYear()} Copyright Text
+            <a class="grey-text text-lighten-4 right" href="!#">Repository</a>
+            </div>
+          </div>
+        </footer>
+    )
+}
